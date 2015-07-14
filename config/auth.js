91b6ca78b8bd03388d7b161ac987d07eebaf4e5f
Mohammad Fahad Sheikh <fahadsheikh780@gmail.com>
2015-07-13 23:22:09 -0700

    Change Auth Callbacks

--- config/auth.js
+++ config/auth.js
@@ -6,19 +6,19 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '446036972230996', // your App ID
         'clientSecret'  : 'cfcf72faed91f8c6d2ed51302f82c5ed', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+        'callbackURL'   : 'https://ilmquest.herokuapp.com/auth/facebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'       : 'w0TtjkhSW7O2TqLtKytK8d2pH',
         'consumerSecret'    : 'Zw5xvZAvTMqxjgS34g7z6mav5KsPeyDY2PcaV4rIVuQHIPn0p3',
-        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+        'callbackURL'       : 'https://ilmquest.herokuapp.com/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'      : '708684690241-718loesppj3udppcrdkh50a8505l8l8b.apps.googleusercontent.com',
         'clientSecret'  : 'W1KqBCcMvHth-2LfutPlr7-J',
-        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        'callbackURL'   : 'https://ilmquest.herokuapp.com/auth/google/callback'
     }
 
 };
