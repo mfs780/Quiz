48c6176ce162eaaf0c26d5d001ee70ad9f3ab0a6
Mohammad Fahad Sheikh <fahadsheikh780@gmail.com>
2015-07-13 21:08:29 -0700

    Change Port Config

--- server.js
+++ server.js
@@ -54,7 +54,9 @@ require('./app/routes')(app, passport); // configure our routes
 
 // start app ===============================================
 // startup our app at http://localhost:8080
-app.listen(port);               
+app.listen(port, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});               
 
 // shoutout to the user                     
 console.log('Magic happens on port ' + port);
