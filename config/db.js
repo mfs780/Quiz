7af5df53ce4c7692ca8608ee1adbcd9c997f9d2e
Mohammad Fahad Sheikh <fahadsheikh780@gmail.com>
2015-07-13 21:16:35 -0700

    MongoLab Changes

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 // config/db.js
 module.exports = {
-    url: 'mongodb://localhost/quizdb'
+    url: 'mongodb://heroku_fqm7rmt8:otutunr5bq70v92f3qlf420j47@ds047802.mongolab.com:47802/heroku_fqm7rmt8'
 }
